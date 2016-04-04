95507637ca77cad083070c05bca4725a78cb5dad
mrjcpu <morganj@mrjcpu.com>
2016-04-04 16:49:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;  botRegexStand = /^\/stand/i;  botRegexTeams = /^\/teams/i;  botRegexSched = /^\/schedule/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;  botRegexStand = /^\/stand/i;  botRegexPlayers = /^\/players/i;  botRegexTeams = /^\/teams/i;  botRegexSched = /^\/schedule/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
@@ -29,7 +29,11 @@ function respond() {
     postMessage("http://daddyleagues.com/NAZ925/standings");
     this.res.end();
   }
-    else if(request.text && botRegexTeams.test(request.text)) {
+  else if(request.text && botRegexPlayers.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/NAZ925/players");
+    this.res.end();
+  }    else if(request.text && botRegexTeams.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://daddyleagues.com/NAZ925/teams");
     this.res.end();
